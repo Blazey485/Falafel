333ebbb7c1c004c9550ba52c4f0e6eb8e5e3fe7e
Niksan <niksan@Niksan-sin-MacBook-Air.local>
2026-08-31 11:42:18 +0200

    npm installed react router and set it up - yet to route the pages. imma just move on to the body, giving up on the header styling is hard ✌️

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,28 @@
 import { useState } from "react";
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/Header.css";
+import { Link } from "react-router";
 
 const Header = () => {
 	return (
 		<div className="main">
 			<div className="header">
 				<h1>🧆 JENS FALAFEL 🧆</h1>
+
 				<nav>
 					<ul>
 						<li>Home</li>
+
 						<li>About Us</li>
+
 						<li>Contact</li>
-						<li>Home</li>
+
+						<li>Login</li>
 					</ul>
 				</nav>
 			</div>
 		</div>
 	);
 };
-
 export default Header;
--- src/root.jsx
+++ src/root.jsx
@@ -2,10 +2,31 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import React from "react";
 import "./css/root.css";
-import App from "./Header.jsx";
 
-createRoot(document.getElementById("root")).render(
+import Header from "./Header.jsx";
+
+import {
+	createBrowserRouter,
+	RouterProvider,
+	Outlet
+} from "react-router";
+
+const RootLayout = () => {
+	return (
+		<div>
+			<Header></Header>;
+		</div>
+	);
+};
+
+const rootLayout = createRoot(
+	document.getElementById("root")
+);
+
+//- Deler dette i 2 for å ha mer oversikt. (render og createRoot)
+
+rootLayout.render(
 	<StrictMode>
-		<App></App>
+		<RootLayout></RootLayout>
 	</StrictMode>
 );
