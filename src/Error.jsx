672f9fada32d83d6b2f60bf7f81b5a60368ca817
Blazey485 <niksandhakal@gmail.com>
2026-09-02 19:46:10 +0200

    added error page, sayin done with it, dont think imma bother styling that

---
+++ src/Error.jsx
@@ -0,0 +1,14 @@
+
+
+const Error = () => {
+	return (
+		<div>
+			<h1>
+				oi, ser ut som vi har ikke noe slikt, kanskje du
+				skrev noe feil?
+			</h1>
+		</div>
+	);
+};
+
+export default Error;
