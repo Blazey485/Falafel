1bc28a2c7e950cade5d7e2923e6ac3f90112ec74
Blazey485 <niksandhakal@gmail.com>
2026-09-13 17:22:36 +0200

    fixed the routing and rendering problem was with the aboutus, forgot capital u in us

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,7 +1,11 @@
 const Body = () => {
 	return (
-		<div>
-			<h1></h1>
+		<div className="Falafel_menu">
+			<br />
+			<br />
+			<h1 className="menu_title">Falafelene vi tilbyr</h1>
+
+			<div className="menu"></div>
 		</div>
 	);
 };
